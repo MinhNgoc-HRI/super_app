743e560a875ac4fdec991742e60074420d72cde8
Minh Ngọc <ngnm1009@gmail.com>
2023-12-01 17:00:22 +0700

    feat(init): init project

---
+++ packages/host/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box, Text, fontSizeLine} from 'pmn-rn-component';
+
+const App = () => {
+  return (
+    <Box middle center flex={1}>
+      <Text size={fontSizeLine(16)}>App</Text>
+    </Box>
+  );
+};
+
+export default App;
